a4c5a044ea1431bcf84d82e067ddf2bc38c214b0
Pedro Paixao <paixaop@gmail.com>
2015-05-24 18:47:36 -0400

    broken

--- client/components/control/bmf-control-directive.js
+++ client/components/control/bmf-control-directive.js
@@ -1,17 +1,12 @@
 
-var module = angular.module('bmfControl', [
-    'angular-meteor',
-    'ngMaterial',
-    'ngMdIcons',
-    'hmTouchEvents'
-  ]);
+var module = angular.module('bmfControl', []);
 
 module.directive('bmfControl', function() {
     return {
       scope: {
         name: '='
       },
-      restrict:  'E',
+
       templateUrl: 'bmf-control-template.ng.html',
       controller: 'BmfControlController'
     };
@@ -21,4 +16,5 @@ module.controller('BmfControlController', ['$scope', '$meteor',
   function($scope, $meteor) {
     var self = this;
     //self.control = $meteor.collection(Controls).find( { "name": $scope.name } );
+    $scope.label = "works";
   }]);
