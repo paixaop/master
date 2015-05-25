e06b65a9090758d07c8aebf9f1a63aed76ef5374
Pedro Paixao <paixaop@gmail.com>
2015-05-25 00:09:01 -0400

    fixed control collection

--- app.js
+++ app.js
@@ -2,5 +2,5 @@
  * Created by pedro on 5/18/15.
  */
 
-Controls = new Mongo.Collection(CONTROLS_COLLECTION_NAME);
+Controls = new Meteor.Collection(CONTROLS_COLLECTION_NAME);
 MqttMessages = new Meteor.Collection(MQTT_MESSAGES_COLLECTION_NAME);
