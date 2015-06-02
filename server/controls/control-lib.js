1d246039454218afea7c75367e5e4bc2175bb559
Pedro Paixao <paixaop@gmail.com>
2015-06-01 22:54:20 -0400

    Controls library initial import

---
+++ server/controls/control-lib.js
@@ -0,0 +1,144 @@
+/**
+ * Control Utility functions
+ * 
+ * Pedro Paixao
+ */
+
+var Control = function() {
+    var self = this;
+    
+
+    /**
+     * Get a control from the database
+     */
+    self.getControlByName = function(controlName) {
+        if (!controlName) {
+            console.log('Error. You must pass a control name ');
+            return undefined;
+        }
+        
+        var c = Controls.find({ name: controlName } );
+        if (!c) {
+            console.log('Control not found: ' + controlName);
+        }
+        else if (!c.type) {
+            console.log('WARNING: Control type unknown, possible control misconfiguration in database.');
+        }
+        
+        return c;
+    };
+    
+    self.loadControlConfigurations = function(name, next) {
+        var config = { };
+        
+        // Read all files in the config directory
+        fs.readdir(Meteor.settings.controls.config_path, function(err, files) {
+            if ( err ) {
+                throw new Error('Control configuration directory not found: ' + err);
+            }
+            angular.forEach(files, function(file) {
+                var m = file.match(/(^[0-9a-z_-]+).json$/i);
+                if( m ) {
+                    self.config[m[1]] = JSON.parse(Assets.getText(Meteor.settings.controls.config_path + file));         
+                }
+            });
+            return next(err);
+        });
+    };
+    
+    /**
+     * Is the property writable
+     */
+    self.isWritable = function(controlSettings, property) {
+        
+        if (!property ||
+            !controlSettings.properties ||
+            !controlSettings.properties[property] ||
+            !controlSettings.properties[property].acl) {
+            
+            return false;
+        }
+        
+        if( controlSettings.properties[property].acl.match(/w/gi) )
+            return true;
+        
+        return false;
+    };
+    
+    /**
+     * Check if a control is of expected type
+     */
+    self.isControlType = function(c, type) {
+        
+        if (!c.type) {
+            console.log('WARNING: Control type unknown, possible control misconfiguration in database.');
+            return false;
+        }
+        
+        var t = type === c.type;
+        if (!t) {
+            console.log('Control '+ c.name + ' does not have the same type ' + type + ' is not ' + c.type);
+        }
+        return t;
+    };
+    
+    self.validateAndParseControlMessage = function(controlSettings, msg) {
+        // control messages are either a direct command like ON or OFF or a parsable JSON object
+        msg.valid = false;
+        
+        if (msg.message.match(controlSettings.basic_commands) ) {
+            msg.type = 'basic';
+            msg.valid = true;
+            return msg;
+        }
+        
+        try {
+            var obj = JSON.parse(msg.message);
+            msg.type = 'json';
+            msg.parse = obj;
+            msg.valid = true;
+        }
+        catch (error) {
+            console.log('Control: Error invalid message in topic ' + msg.topic);
+        }
+        
+        return msg;
+    };
+    
+};
+
+/*
+var Switch = function() {
+    var self = this;
+    
+    self.processMessage = function(msg) { 
+        var controlSettings = { };
+        controlSettings = JSON.parse(Assets.getText("control.json"));
+        
+        var control = getControlByName(controlSettings, msg.params.name)
+        
+        if (!control) {
+            return;
+        }
+        
+        if( !isControlType(control.type, 'switch') ){
+            return;
+        }
+    
+        if (control.path !== msg.params.path) {
+            console.log('WARNING! Control ' + msg.params.name + ' found but there\'s a path mismatch. Expecting ' +
+                control.path + ' got ' + msg.params.name + '. Ignoring path');
+        }
+    
+        
+    }
+    
+    self.isValidMessage = function(msg) {
+        // Validate message before processing it
+    }
+    
+}
+
+control.register('switch', Switch);
+
+*/
