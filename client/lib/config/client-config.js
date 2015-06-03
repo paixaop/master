76f1d58b1dcf6b727f6f806aab0883184018083e
Pedro Paixao <paixaop@gmail.com>
2015-06-03 02:50:50 -0400

    clientConfig

--- client/lib/config/client-config.js
+++ client/lib/config/client-config.js
@@ -2,5 +2,18 @@ clientConfig = {
     
     angular: {
         app_name: "master"
+    },
+    
+    controls: {
+        collection: "controls",
+        
+        // Single Tap idenfication (in ms)
+        // A single tap will be identified if control taps are separated by this interval
+        tap_delay: 500,
+        
+        // Double tap
+        // A doulbe tap will be identified if control taps are separated by this interval
+        // double_tap delay must be smaller than tap_delay
+        doubletap_delay: 200
     }
 };
