6cce6b8cb80e217d90643aca6c215f07aef62412
Pedro Paixao <paixaop@gmail.com>
2015-06-03 21:30:24 -0400

    comments and new settings

--- server/lib/config/config.js
+++ server/lib/config/config.js
@@ -16,8 +16,25 @@ serverConfig.mqtt = {
         max_message_length: 1000,
         
         // Limit the maximum size of a MQTT topic. Messages sent on longer topics are ignored
-        max_topic_length: 1000
+        max_topic_length: 1000,
+        
+        // Some parts of the MQTT topic are used as parameters to identify control names or
+        // system routes, the following settings configure the validation of said parameters
+        // after they are extracted from the topic string.
+        
+        // Regular expressions that validate the route parameter extracted from MQTT topic
+        // if any of the regular expressions matches then route is considered valid
+        valid_routes: [
+            /^(?:control|screen|panel)$/
+        ],
+        
+        // Maximum lenght of the name topic parameter
+        max_name_length: 200,
+        
+        // Maximum length of the route topic parameter
+        max_route_length: 100
     },
+    
     
     // MQTT brokers
     brokers: {
@@ -41,9 +58,18 @@ serverConfig.mqtt = {
             // QoS 0, 1, 2
             topic_qos: 0,
             
-            // Paterns to match toic structure and extract named parameters
+            /* Patterns to match topic structure and extract named parameters.
+               Pattern Requirements:
+            
+                 * You MUST define `+name` as one of the parameters, as it will be used to 
+                   retrieve the control from the database
+                   
+                 * You MUST define `+route` as one of the parameters. Route is used
+                   to identify the message handler, control, screen or panel.
+                   
+            */
             patterns: [
-               "master/+thing/+name/+ctrl/#path"
+               "master/+route/+name/+cmd/#path"
             ]
         }    
     }
@@ -58,7 +84,7 @@ serverConfig.controls = {
     security: {
         
         // Limit the maximum size of a MQTT message. Bigger messages are ignored
-        max_control_name_length: 200
+        max_name_length: 200
     }
 };
 
