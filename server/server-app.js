d2d23ea912ad7a95ef6165f8d105e45f1f7523db
Pedro Paixao <paixaop@gmail.com>
2015-06-05 13:52:55 -0400

    fix master var name

--- server/server-app.js
+++ server/server-app.js
@@ -47,7 +47,8 @@ Meteor.startup(function() {
     }
 
     if( msg.params["route"] === 'control' ) {
-      msg = Master.Control.processMessage(msg);
+      console.log('Routing to control module');
+      msg = master.Control.processMessage(msg);
     }
     else {
       throw new Meteor.Error('Error. Unknown handler for route: ' + msg.params[route]);
